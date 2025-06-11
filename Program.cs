5b56023358cab87e35851482f3c48b149f337a56
dayul <dyulchoo@naver.com>
2025-06-11 10:12:31 +0900

    강아지, 고양이 클래스 예제 (상속 적용 전)

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace PolyExample
     {
         static void Main(string[] args)
         {
+            // List 변수명은 복수로
+            List<Dog> Dogs = new List<Dog>() {new Dog(), new Dog(), new Dog()};
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+
+            // var는 타입 추론
+            foreach(var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
+
         }
     }
 }
